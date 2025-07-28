cb6318b47072c7c798f1445c7515f03e12555c11
YashSewarik <yashsewarik1@gmail.com>
2025-07-28 23:54:44 +0530

    Added the Navbar

--- ecommerce/src/App.jsx
+++ ecommerce/src/App.jsx
@@ -1,12 +1,33 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
+import Collections from './pages/Collections'
+import About from './pages/About';
+import Contacts from './pages/Contacts';
+import Product from './pages/Product';
+import Cart from './pages/Cart';
+import Login from './pages/Login';
+import PlaceOrder from './pages/PlaceOrder';
+import Orders from './pages/Orders';
+import Navbar from './components/Navbar';
+
+
 
 const App = () => {
   return (
     <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <Navbar />
       <Routes>
         <Route path='/' element={<Home/>} />
+        <Route path ='/collection' element={<Collections/>}   />
+        <Route path="/about" element={<About/>}/>
+        <Route path="/contact" element={<Contacts/>}/>
+        <Route path="/product/:productId" element={<Product/>}/>
+        <Route path="/cart" element={<Cart/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/placeOrder" element={<PlaceOrder/>}/>
+        <Route path="/orders" element={<Orders/>}/>
+
       </Routes>
     </div>
 
