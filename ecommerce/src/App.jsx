912d42d70ab25abdc60aa83210b124c9bb86087d
YashSewarik <yashsewarik1@gmail.com>
2025-07-28 22:23:10 +0530

    Created Page structure

--- ecommerce/src/App.jsx
+++ ecommerce/src/App.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+      </Routes>
+    </div>
+
   )
 }
 
