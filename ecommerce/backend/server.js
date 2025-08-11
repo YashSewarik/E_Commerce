1c5fea858d732506ee2b9d2d56498bda0ffe87e1
YashSewarik <yashsewarik1@gmail.com>
2025-08-11 22:50:08 +0530

    Added backend routes and database

--- ecommerce/backend/server.js
+++ ecommerce/backend/server.js
@@ -1,17 +1,24 @@
+import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
-import 'dotenv/config';
+import connectDB from './config/mongodb.js';
+import connectCloudinary from './config/cloudinary.js';
+import userRouter from './routes/userRoute.js';
 
 //app config
 
 const app = express();
 const port = process.env.PORT||4000;
+connectDB();
+connectCloudinary();
 
 // middlewares 
 app.use(express.json())
 app.use(cors())
 
 //api end points
+
+app.use('/api/user',userRouter);
 
 app.get('/',(req,res)=>{
     res.send("API Working")
