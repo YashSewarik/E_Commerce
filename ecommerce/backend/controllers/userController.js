2f1e7086de0981a5436d3d3a5e775915fa278205
YashSewarik <yashsewarik1@gmail.com>
2025-08-13 12:16:35 +0530

    Added admin login api

--- ecommerce/backend/controllers/userController.js
+++ ecommerce/backend/controllers/userController.js
@@ -76,6 +76,25 @@ const registerUser = async (req,res)=>{
 
 const adminLogin = async (req,res)=>{
 
+    try {
+        
+        const {email,password} = req.body;
+        if (email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD) {
+            const token = jwt.sign(email+password,process.env.JWT_SECRET)
+            res.json({success:true,token})
+        }
+        else{
+            res.json({success:false,message:'Invalid Credentials'})
+        }
+
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({
+            success: false,
+            message: error.message || "Server error"
+        });
+    }
+
 }
 
 export {loginUser,registerUser,adminLogin};
